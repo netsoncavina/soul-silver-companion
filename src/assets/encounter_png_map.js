cf3e43a9b93914e37dc24a5115d989d2b387f79d
Netson Cavina <netsoncavina@gmail.com>
2025-05-07 12:52:22 -0300

    map dos encontros

---
+++ src/assets/encounter_png_map.js
@@ -0,0 +1,35 @@
+import BlainePNG from './images/encounters/dojo16.png';
+import BluePNG from './images/encounters/dojo17.png';
+import BrockPNG from './images/encounters/dojo10.png';
+import BugsyPNG from './images/encounters/dojo03.png';
+import ChuckPNG from './images/encounters/dojo06.png';
+import ClairPNG from './images/encounters/dojo09.png';
+import ErikaPNG from './images/encounters/dojo13.png';
+import FalknerPNG from './images/encounters/dojo02.png';
+import JaninePNG from './images/encounters/dojo14.png';
+import JasminePNG from './images/encounters/dojo07.png';
+import LtSurgePNG from './images/encounters/dojo12.png';
+import MistyPNG from './images/encounters/dojo11.png';
+import MortyPNG from './images/encounters/dojo05.png';
+import PrycePNG from './images/encounters/dojo08.png';
+import SabrinaPNG from './images/encounters/dojo15.png';
+import WhitneyPNG from './images/encounters/dojo04.png';
+
+export const trainerPNGMap = {
+  Blaine: BlainePNG,
+  Blue: BluePNG,
+  Brock: BrockPNG,
+  Bugsy: BugsyPNG,
+  Chuck: ChuckPNG,
+  Clair: ClairPNG,
+  Erika: ErikaPNG,
+  Falkner: FalknerPNG,
+  Janine: JaninePNG,
+  Jasmine: JasminePNG,
+  'Lt. Surge': LtSurgePNG,
+  Misty: MistyPNG,
+  Morty: MortyPNG,
+  Pryce: PrycePNG,
+  Sabrina: SabrinaPNG,
+  Whitney: WhitneyPNG,
+};
