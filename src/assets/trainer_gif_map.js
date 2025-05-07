c1ed9834bdf61c70261296249319ebef3d030916
Netson Cavina <netsoncavina@gmail.com>
2025-05-07 03:36:55 -0300

    mapeamento dos gifs

---
+++ src/assets/trainer_gif_map.js
@@ -0,0 +1,35 @@
+import BlaineGif from './gifs/BlaineHGSS.gif';
+import BlueGif from './gifs/BlueHGSS.gif';
+import BrockGif from './gifs/BrockHGSS.gif';
+import BugsyGif from './gifs/BugsyHGSS.gif';
+import ChuckGif from './gifs/ChuckHGSS.gif';
+import ClairGif from './gifs/ClairHGSS.gif';
+import ErikaGif from './gifs/ErikaHGSS.gif';
+import FalknerGif from './gifs/FalknerHGSS.gif';
+import JanineGif from './gifs/JanineHGSS.gif';
+import JasmineGif from './gifs/JasmineHGSS.gif';
+import LtSurgeGif from './gifs/LtSurgeHGSS.gif';
+import MistyGif from './gifs/MistyHGSS.gif';
+import MortyGif from './gifs/MortyHGSS.gif';
+import PryceGif from './gifs/PryceHGSS.gif';
+import SabrinaGif from './gifs/SabrinaHGSS.gif';
+import WhitneyGif from './gifs/WhitneyHGSS.gif';
+
+export const trainerGifMap = {
+  Blaine: BlaineGif,
+  Blue: BlueGif,
+  Brock: BrockGif,
+  Bugsy: BugsyGif,
+  Chuck: ChuckGif,
+  Clair: ClairGif,
+  Erika: ErikaGif,
+  Falkner: FalknerGif,
+  Janine: JanineGif,
+  Jasmine: JasmineGif,
+  'Lt. Surge': LtSurgeGif,
+  Misty: MistyGif,
+  Morty: MortyGif,
+  Pryce: PryceGif,
+  Sabrina: SabrinaGif,
+  Whitney: WhitneyGif,
+};
