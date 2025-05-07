a48a68eb1ab05c2ca54fedbaa7717b9ddedb18a4
Netson Cavina <netsoncavina@gmail.com>
2025-05-07 00:48:15 -0300

    melhorias no componente de notificação

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,36 +1,82 @@
 import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import CloseIcon from '@mui/icons-material/Close';
 import { FaBell } from 'react-icons/fa';
+import useBattleNotifications from '../hooks/useBattleNotifications';
 
-function Notification({ notifications = [] }) {
-  const onClick = () => {
-    console.log('Notification clicked', notifications);
+export default function NotificationMenu() {
+  const { notifications, updateNotifications, removeNotification } =
+    useBattleNotifications();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e) => {
+    updateNotifications();
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <div
-      style={{ position: 'relative', display: 'inline-block' }}
-      onClick={onClick}
-    >
-      <FaBell size={30} color='#f7c029' />
-      {notifications.length > 0 && (
-        <span
-          style={{
-            position: 'absolute',
-            top: '-5px',
-            right: '-5px',
-            backgroundColor: 'red',
-            color: 'white',
-            borderRadius: '50%',
-            padding: '2px 6px',
-            fontSize: '16px',
-            fontWeight: 'bold',
-          }}
+    <>
+      <IconButton
+        onClick={handleClick}
+        aria-controls={open ? 'notification-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
+      >
+        <Badge
+          badgeContent={notifications.length}
+          color='error'
+          overlap='circular'
         >
-          {notifications.length}
-        </span>
-      )}
-    </div>
+          <FaBell size={24} color='#f7c029' />
+        </Badge>
+      </IconButton>
+
+      <Menu
+        id='notification-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        MenuListProps={{ 'aria-labelledby': 'notification-button' }}
+      >
+        {notifications.length > 0 ? (
+          notifications.map((msg, i) => (
+            <MenuItem
+              key={i}
+              onClick={handleClose}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <ListItemText>{msg}</ListItemText>
+              <IconButton
+                size='small'
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeNotification(i);
+                }}
+              >
+                <CloseIcon fontSize='small' />
+              </IconButton>
+            </MenuItem>
+          ))
+        ) : (
+          <MenuItem onClick={handleClose}>Nenhuma notificação</MenuItem>
+        )}
+      </Menu>
+    </>
   );
 }
-
-export default Notification;
